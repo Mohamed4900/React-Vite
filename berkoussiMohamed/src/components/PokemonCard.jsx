4ab5199d339e7bc839aab611c74cf367d7f8c0ea
Mohamed4900 <Mohamedibnlakhdar10@gmail.com>
2023-03-28 10:29:54 +0200

    Bulbasaur est là !

---
+++ berkoussiMohamed/src/components/PokemonCard.jsx
@@ -0,0 +1,9 @@
+
+function PokemonCard() {
+    return  <figure>
+                <img src="https://www.pokepedia.fr/images/e/ef/Bulbizarre-RFVF.png"/>
+                <figcaption>Bulbasaur</figcaption>
+            </figure>;
+}
+
+export default PokemonCard;
