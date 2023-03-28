30414f846c8f43f7887730f1b2ea1c7e2595c745
Mohamed4900 <Mohamedibnlakhdar10@gmail.com>
2023-03-28 11:52:11 +0200

    React Basics 03 est fait

--- berkoussiMohamed/src/components/PokemonCard.jsx
+++ berkoussiMohamed/src/components/PokemonCard.jsx
@@ -1,9 +1,36 @@
+import { useState } from "react";
+
 
 function PokemonCard() {
-    return  <figure>
-                <img src="https://www.pokepedia.fr/images/e/ef/Bulbizarre-RFVF.png"/>
-                <figcaption>Bulbasaur</figcaption>
-            </figure>;
+const pokemonList = [
+    {
+        name: "Bulbasaur",
+        type: "Plante/Poison",
+        imgSrc: "https://www.pokepedia.fr/images/e/ef/Bulbizarre-RFVF.png",
+    },
+    {
+        name: "Charmander",
+        type: "Feu",
+        imgSrc: "https://www.pokepedia.fr/images/8/89/Salam%C3%A8che-RFVF.png",
+    },
+]
+
+const [pokemon, setPokemon] = useState(pokemonList[0]);
+const changePokemon = () => {
+    setPokemon(pokemonList[1]);
+};
+return (
+    <div>
+        <h1>{pokemon.name}</h1>
+        <p>Type: {pokemon.type}</p>
+        {pokemon.imgSrc ? (
+            <img className="avatar" src={pokemon.imgSrc} alt={pokemon.name} />
+        ) : (
+            <p>???</p>
+        )}
+        <button onClick={changePokemon}>Change Pokemon !!!</button>
+    </div>
+);
 }
 
 export default PokemonCard;
