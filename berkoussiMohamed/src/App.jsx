f4e567609784f9435f6bb0592e4f9dd29e949695
Mohamed4900 <Mohamedibnlakhdar10@gmail.com>
2023-03-29 11:53:33 +0200

    React Basics 04 - Pokédex - Passer des props est fait !

--- berkoussiMohamed/src/App.jsx
+++ berkoussiMohamed/src/App.jsx
@@ -3,15 +3,19 @@ import MyTitle from "./components/MyTitle"
 import PokemonCard from "./components/PokemonCard"
 
 function App() {
+  const pokemonList = [
+    {
+      name: "Bulbasaur", 
+      type: "Plante/Poison",
+      imgSrc: "https://www.pokepedia.fr/images/e/ef/Bulbizarre-RFVF.png",
+    },
+    {
+      name: "meow",
+    },
+  ];
 
-  return (
-    <div>
-      <MyTitle />
-      <div>
-        <PokemonCard />
-      </div>
-    </div>
-  )
-}
+  const pokemon = pokemonList[0]
+  return <PokemonCard pokemon={pokemon}/>
+};
 
-export default App
+export default App;
--- berkoussiMohamed/src/components/PokemonCard.jsx
+++ berkoussiMohamed/src/components/PokemonCard.jsx
@@ -1,36 +1,17 @@
-import { useState } from "react";
+import React from 'react';
 
-
-function PokemonCard() {
-const pokemonList = [
-    {
-        name: "Bulbasaur",
-        type: "Plante/Poison",
-        imgSrc: "https://www.pokepedia.fr/images/e/ef/Bulbizarre-RFVF.png",
-    },
-    {
-        name: "Charmander",
-        type: "Feu",
-        imgSrc: "https://www.pokepedia.fr/images/8/89/Salam%C3%A8che-RFVF.png",
-    },
-]
-
-const [pokemon, setPokemon] = useState(pokemonList[0]);
-const changePokemon = () => {
-    setPokemon(pokemonList[1]);
+function PokemonCard({pokemon}) {
+    const { name, type, imgSrc} = pokemon;
+    return (
+        <div>
+            <h1>{name}</h1>
+            <div className='card'>
+            <img className="avatar" src={imgSrc}/>
+            </div>
+            <p>Type: {type}</p>
+        </div>
+    );
 };
-return (
-    <div>
-        <h1>{pokemon.name}</h1>
-        <p>Type: {pokemon.type}</p>
-        {pokemon.imgSrc ? (
-            <img className="avatar" src={pokemon.imgSrc} alt={pokemon.name} />
-        ) : (
-            <p>???</p>
-        )}
-        <button onClick={changePokemon}>Change Pokemon !!!</button>
-    </div>
-);
-}
+
 
 export default PokemonCard;
