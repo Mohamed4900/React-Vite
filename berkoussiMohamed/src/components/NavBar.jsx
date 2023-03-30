c1c1fe9329c22cc754735ba298c589f40cb9ef55
Mohamed4900 <Mohamedibnlakhdar10@gmail.com>
2023-03-30 12:16:47 +0200

    React Basics 07 - Pokédex est fait !

---
+++ berkoussiMohamed/src/components/NavBar.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+function NavBar() {
+        <nav>
+            <button onClick={handleClick1}>Suivant</button>
+            <button onClick={handleClick2}>Retour</button>
+        </nav>
+};
+
+
+export default NavBar;
